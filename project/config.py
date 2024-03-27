608f1b5c1370ff1e4869214d13b403cdf8f9f5e2
Rafin000 <marufulislam00000@gmail.com>
2024-03-27 11:41:44 +0600

    Setting up auto reload done

--- project/config.py
+++ project/config.py
@@ -13,7 +13,7 @@ class BaseConfig:
 
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
     CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")  
-
+    # CELERY_TASK_ALWAYS_EAGER=True
 
 
 class DevelopmentConfig(BaseConfig):
